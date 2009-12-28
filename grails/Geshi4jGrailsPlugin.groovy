0aa0b228ec276ac6c2d35ab21dadf7276f3f1772
Aaron Collegeman <aaron@collegeman.net>
2009-12-28 00:21:55 -0500

    Committing version 1.0.0

--- grails/Geshi4jGrailsPlugin.groovy
+++ grails/Geshi4jGrailsPlugin.groovy
@@ -1,12 +1,12 @@
 class Geshi4jGrailsPlugin {
     // the plugin version
-    def version = "0.1.0"
+    def version = "1.0.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1.2 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
-    def pluginExcludes = [ "grails-app/views/error.gsp" ]
+    def pluginExcludes = [ "grails-app/views/error.gsp", "grails-app/views/index.gsp" ]
 
     // TODO Fill in these fields
     def author = "Aaron Collegeman"
